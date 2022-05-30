ff649fa78b304514d7545ce28fc7eb501fa42fb1
Andersom <andersom.rfd@hotmail.com>
2022-05-30 11:57:03 -0300

    product model criado

---
+++ lib/app/data/models/product.dart
@@ -0,0 +1,27 @@
+class ProductModel {
+
+  int id;
+  String name;
+  num price;
+  String unitOfMeasure;
+  String? description;
+  String image;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.unitOfMeasure,
+    required this.image,
+    this.description,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+    id: json['id'],
+    name: json['nome'],
+    price: json['preco'],
+    unitOfMeasure: json['unidade'],
+    image: json['imagem'],
+    description: json['descricao'],
+  );
+}
