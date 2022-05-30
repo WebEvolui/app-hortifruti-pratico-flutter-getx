8e5e2afb397baefcea2fcb6525e7b8957cee4048
Andersom <andersom.rfd@hotmail.com>
2022-05-30 15:02:27 -0300

    Adicionado as categorias e os produtos na tela do estabelecimento

--- lib/app/modules/store/page.dart
+++ lib/app/modules/store/page.dart
@@ -2,6 +2,7 @@ import 'package:app_hortifruti_pratico/app/modules/store/controller.dart';
 import 'package:app_hortifruti_pratico/app/widgets/store_status.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class StorePage extends GetView<StoreController> {
@@ -15,11 +16,17 @@ class StorePage extends GetView<StoreController> {
             const SliverAppBar(),
             SliverToBoxAdapter(
               child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  top: 8.0,
+                  right: 16.0,
+                  bottom: 16.0
+                ),
                 child: Row(
                   children: [
                     SizedBox(
                       width: 96.0,
+                      height: 96.0,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         child: FadeInImage.memoryNetwork(
@@ -44,6 +51,54 @@ class StorePage extends GetView<StoreController> {
                     ),
                   ],
                 ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                ((context, index) {
+                  var category = state.categories[index];
+
+                  return Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              color: Colors.grey[200],
+                              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                              child: Text(
+                                category.name,
+                                style: Get.textTheme.titleMedium!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          )
+                        ],
+                      ),
+                      for (var product in category.products)
+                        ListTile(
+                          title: Text(product.name),
+                          subtitle: Text(NumberFormat.simpleCurrency().format(product.price)),
+                          leading: product.image.isNotEmpty
+                              ? SizedBox(
+                                  width: 56.0,
+                                  height: 56.0,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    child: FadeInImage.memoryNetwork(
+                                      placeholder: kTransparentImage,
+                                      image: product.image,
+                                    ),
+                                  ),
+                                )
+                              : null,
+                          onTap: () {},
+                        )
+                    ],
+                  );
+                }),
+                childCount: state.categories.length
               ),
             )
           ],
