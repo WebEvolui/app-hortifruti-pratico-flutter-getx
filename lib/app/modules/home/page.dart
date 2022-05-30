a301d3718f4c825614fa7c76b4ccc39ea79898e0
Andersom <andersom.rfd@hotmail.com>
2022-05-29 22:13:54 -0300

    Alterado o visual do item da lista de estabelecimentos

--- lib/app/modules/home/page.dart
+++ lib/app/modules/home/page.dart
@@ -1,6 +1,7 @@
 import 'package:app_hortifruti_pratico/app/modules/home/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends GetView<HomeController> {
 
@@ -14,8 +15,33 @@ class HomePage extends GetView<HomeController> {
             for (var store in state!)
               ListTile(
                 title: Text(store.name),
-                leading: FlutterLogo(),
-                trailing: Text(store.isOnline ? 'Aberto' : 'Fechado'),
+                leading: Container(
+                  width: 56.0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: store.image,
+                    ),
+                  ),
+                ),
+                trailing: Container(
+                  padding: const EdgeInsets.all(4.0),
+                  decoration: BoxDecoration(
+                    color: store.isOnline ? Colors.green : Colors.black45,
+                    border: Border.all(color: Colors.black12, width: 2.0),
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: Text(
+                    store.isOnline ? 'Aberto' : 'Fechado',
+                    style: Get.textTheme.bodyMedium!.copyWith(color: Colors.white),
+                  ),
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 16.0
+                ),
+                onTap: () {},
               )
           ],
         ),
