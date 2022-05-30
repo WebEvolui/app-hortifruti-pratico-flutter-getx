5ef23462c63c18d75b785f9482cb86ff06ec8e3e
Andersom <andersom.rfd@hotmail.com>
2022-05-30 14:33:15 -0300

    ListView alterado para CustomScrollView

--- lib/app/modules/store/page.dart
+++ lib/app/modules/store/page.dart
@@ -9,39 +9,41 @@ class StorePage extends GetView<StoreController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: controller.obx(
-        (state) => ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: Row(
-                children: [
-                  Container(
-                    width: 96.0,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: FadeInImage.memoryNetwork(
-                        placeholder: kTransparentImage,
-                        image: state!.image,
+        (state) => CustomScrollView(
+          slivers: [
+            const SliverAppBar(),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 96.0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage,
+                          image: state!.image,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          state.name,
-                          style: Get.textTheme.headline5,
-                        ),
-                        const SizedBox(height: 8.0,),
-                        StoreStatus(isOnline: state.isOnline)
-                      ]
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            state.name,
+                            style: Get.textTheme.headline5,
+                          ),
+                          const SizedBox(height: 8.0,),
+                          StoreStatus(isOnline: state.isOnline)
+                        ]
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             )
           ],
