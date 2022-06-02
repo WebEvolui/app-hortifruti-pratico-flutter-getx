5aff4ff98cc5212701e2e649827b1ab9ec124e85
Andersom <andersom.rfd@hotmail.com>
2022-06-02 20:17:12 -0300

    Adiciona o produto no carrinho

--- lib/app/modules/product/controller.dart
+++ lib/app/modules/product/controller.dart
@@ -1,5 +1,8 @@
+import 'package:app_hortifruti_pratico/app/data/models/cart_product.dart';
 import 'package:app_hortifruti_pratico/app/data/models/product.dart';
 import 'package:app_hortifruti_pratico/app/data/models/store.dart';
+import 'package:app_hortifruti_pratico/app/data/services/cart/service.dart';
+import 'package:app_hortifruti_pratico/app/modules/product/widgets/quantity_and_weight/quantity_and_weight_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,6 +11,7 @@ class ProductController extends GetxController {
   final product = Rxn<ProductModel>();
   final store = Rxn<StoreModel>();
   final observationController = TextEditingController();
+  final _cartService = Get.find<CartService>();
 
   @override
   void onInit() {
@@ -18,6 +22,25 @@ class ProductController extends GetxController {
   }
 
   void addToCart() {
-    observationController.text;
+    var quantity = Get.find<QuantityAndWeightController>().quantity;
+
+    _cartService.addProductToCart(
+      CartProductModel(
+        product: product.value!,
+        quantity: quantity,
+        observation: observationController.text
+      )
+    );
+
+    ScaffoldMessenger.of(Get.overlayContext!).showSnackBar(
+      SnackBar(
+        content: Text('O item ' + product.value!.name + ' foi adicionado no carrinho.'),
+      )
+    );
+
+    Future.delayed(
+      const Duration(milliseconds: 300),
+      () => Get.back()
+    );
   }
 }
