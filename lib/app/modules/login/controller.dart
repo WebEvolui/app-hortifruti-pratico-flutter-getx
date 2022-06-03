1b0782ea105205862af8e6f3b52e23a7f1d74a21
Andersom <andersom.rfd@hotmail.com>
2022-06-03 18:17:43 -0300

    Criado os campos do formulário do login

--- lib/app/modules/login/controller.dart
+++ lib/app/modules/login/controller.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
 
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+
+  void login() {
+    
+  }
 }
--- lib/app/modules/login/page.dart
+++ lib/app/modules/login/page.dart
@@ -8,7 +8,62 @@ class LoginPage extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('LoginPage')),
-      body: Text('LoginPage')
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller.emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+              validator: (String? value) {
+                if (value != null && value.isEmpty) {
+                  return 'Informe o seu email';
+                }
+
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: controller.passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Senha',
+              ),
+              validator: (String? value) {
+                if (value != null && value.isEmpty) {
+                  return 'Informe a sua senha';
+                }
+
+                return null;
+              },
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: controller.login,
+                      child: Text('Entrar')
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: Text('Quero criar a minha conta')
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 }
