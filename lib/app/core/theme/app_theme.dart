03f9c2a428090fe1e46b7865b66e1decb2cf9128
Andersom <andersom.rfd@hotmail.com>
2022-06-14 00:34:34 -0300

    ElevatedButton personalizado

--- lib/app/core/theme/app_theme.dart
+++ lib/app/core/theme/app_theme.dart
@@ -8,8 +8,16 @@ var radioTheme = RadioThemeData(
   fillColor: MaterialStateColor.resolveWith((states) => colorScheme.primary),
 );
 
+var elevatedButtonTheme = ElevatedButtonThemeData(
+  style: ElevatedButton.styleFrom(
+    onPrimary: colorScheme.onPrimary,
+    primary: colorScheme.primary
+  )
+);
+
 final ThemeData themeData = ThemeData(
   colorScheme: colorScheme,
   radioTheme: radioTheme,
+  elevatedButtonTheme: elevatedButtonTheme,
   useMaterial3: true,
 );
